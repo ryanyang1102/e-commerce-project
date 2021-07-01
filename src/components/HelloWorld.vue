93e2b9bae233cf484cf62ef7bea0f888b61ffe0a
Ryan Yang <ryan.yang1102@gmail.com>
2021-07-01 17:01:58 +0800

    try bootstrap customized

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -95,7 +95,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped>  /* scoped 代表此設定只限目前元件檔案 */
 h1, h2 {
   font-weight: normal;
 }
