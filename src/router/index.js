cf32edea5523613e6110cd78054840d67f351e15
Ryan Yang <ryan.yang1102@gmail.com>
2021-07-02 13:09:38 +0800

    set up signin & signout

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import HelloWorld from '@/components/HelloWorld';
+import Login from '@/components/pages/Login';
 
 Vue.use(Router)
 
@@ -9,7 +11,12 @@ export default new Router({
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+      component: HelloWorld,
+    },
+    {
+      path: '/login',        // 路徑儘量打小寫
+      name: 'Login',
+      component: Login,
+    },
+  ],
+});
